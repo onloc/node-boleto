6cc8e3321c1be02ab1563ca29d2b800f9d16642e
Renan <renan@codetab.com.br>
2023-09-13 19:23:53 -0300

    feat: some changes

--- examples/safra-emission.js
+++ examples/safra-emission.js
@@ -12,13 +12,13 @@ var boleto = new Boleto({
   'valor': '62998',
   'nosso_numero': '6',
   'numero_documento': '123456789',
-  'cedente': 'Onloc - Locação de Computadores Ltda',
+  'cedente': 'Onloc - Locação de Computadores Ltda - 16.700.797/0001-70',
   'cedente_cnpj': '16.700.797/0001-70',
-  'agencia': '99999',
-  'codigo_cedente': '009999999',
-  'carteira': '25',
-  'pagador': '4 IDEA COMERCIO, IMPORTACAO E EXPORTACAO DE PRODUTOS MEDICOS E HOSPITALARES LTDA\nCPF: 31.277.655/0001-35',
-  'local_de_pagamento': 'PAGÁVEL EM QUALQUER BANCO ATÉ O VENCIMENTO.',
+  'agencia': '99990',
+  'codigo_cedente': '0008554440',
+  'carteira': '1',
+  'pagador': '4 IDEA COMERCIO, IMPORTACAO E EXPORTACAO DE PRODUTOS MEDICOS E HOSPITALARES LTDA\nCPF: 31.277.655/0001-35\nR JOAO DE SOUZA COELHO, 286, PRQ VIA N13065703 CAMPINAS SP',
+  'local_de_pagamento': 'Pagável em qualquer Banco do sistema de compensação.',
   'instrucoes': 'Sr. Caixa, aceitar o pagamento e não cobrar juros após o vencimento.',
   //'barcode': '42295946200000250227124000085544404180276562'
   barcode: '42299706400000629987999990099999991234567892'
@@ -28,7 +28,8 @@ var boleto = new Boleto({
 
 app.use(express.static(path.join(__dirname, '/../')))
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) { 
+
   boleto.renderHTML(function (html) {
     return res.send(html)
   })
